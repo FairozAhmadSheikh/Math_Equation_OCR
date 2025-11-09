d8eecb96b6bd5d57ed1e594c3aa632b145116262
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-11-09 11:56:38 +0530

    image_to_base64

--- app.py
+++ app.py
@@ -54,3 +54,8 @@ def save_file(file_storage):
     path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
     file_storage.save(path)
     return filename, path
+
+def image_to_base64(path):
+    with open(path, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode("utf-8")
